2f0e0ba38be1a7470820b4fce3eff80779487e9d
Mandil Malla <mandilmallaprog92@gmail.com>
2021-08-02 12:43:21 +0900

    simple responsive website

---
+++ js/index.js
@@ -0,0 +1,9 @@
+burger = document.querySelector('.burger');
+navbar = document.querySelector('.navbar');
+navlist = document.querySelector('.nav-list');
+rightNav = document.querySelector('.rightNav');
+burger.addEventListener('click', () => {
+    navbar.classList.toggle('h-nav');
+    navlist.classList.toggle('v-class');
+    rightNav.classList.toggle('v-class')
+})
